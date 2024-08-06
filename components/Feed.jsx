a075bcd9136c0b29ad99d48ca2420030dc65a3e7
youssefwaheed123 <youssefwaheed@gmail.com>
2024-08-06 17:03:01 +0300

    yarab

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -1,3 +1,23 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import PromptCard from "./PromptCard";
+
+
+const PromptCardList = ({ data, handleTagClick }) => {
+  return (
+    <div className="mt-16 prompt_layout">
+      {data.map((post) => (
+        <PromptCard
+          key={post._id}
+          post={post}
+          handleTagClick={handleTagClick}
+        />
+      ))}
+    </div>
+  );
+};
+
 const Feed = () => {
   const [searchText, setSearchText] = useState("");
   const [posts, setPosts] = useState([]);
@@ -49,3 +69,5 @@ const Feed = () => {
     </section>
   );
 };
+
+export default Feed;
