3291ba8882081cc43b45ac05f626a18c44cdd275
youssefwaheed123 <youssefwaheed@gmail.com>
2024-08-06 15:33:22 +0300

    yarab ba

--- app/api/prompt/route.js
+++ app/api/prompt/route.js
@@ -1,24 +1,14 @@
 import { connectToDB } from "@utils/database";
 import Prompt from "@models/prompt";
 
-export const GET = async (request) => {
+export const GET = async (req) => {
   try {
     await connectToDB();
 
-    // Ensure that the connection is refreshed each time
-    const prompts = await Prompt.find({}).populate("creator").exec();
-    console.log("Fetched prompts:", prompts); // Debugging log
-
-    return new Response(JSON.stringify(prompts), {
-      status: 200,
-      headers: {
-        "Cache-Control": "no-cache, no-store, must-revalidate",
-        Pragma: "no-cache",
-        Expires: "0",
-      },
-    });
+    const prompts = await Prompt.find();
+    if (!prompts) return new Response("Prompts not found", { status: 404 });
+    return new Response(JSON.stringify(prompts), { status: 200 });
   } catch (err) {
-    console.error("Error fetching prompts:", err); // Debugging log
     return new Response("Failed to fetch all prompts", { status: 500 });
   }
 };
