8a1cab770c23397217deb5295634844ea116403a
youssefwaheed123 <youssefwaheed@gmail.com>
2024-08-07 01:13:11 +0300

    test

--- app/api/prompt/route.js
+++ app/api/prompt/route.js
@@ -5,20 +5,12 @@ export const GET = async (req) => {
   try {
     await connectToDB();
 
-    const prompts = await Prompt.find().populate("creator").lean().exec();
+    const prompts = await Prompt.find().populate("creator");
 
     if (!prompts) return new Response("Prompts not found", { status: 404 });
 
     return new Response(JSON.stringify(prompts), {
       status: 200,
-      headers: {
-        "Content-Type": "application/json",
-        "Cache-Control":
-          "no-store, no-cache, must-revalidate, proxy-revalidate",
-        Pragma: "no-cache",
-        Expires: "0",
-        "Surrogate-Control": "no-store",
-      },
     });
   } catch (err) {
     console.error("Error fetching prompts:", err.message);
