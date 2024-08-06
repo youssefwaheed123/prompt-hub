fd44f5dcce7a693dd618e1a870e676dc811b6d1e
youssefwaheed123 <youssefwaheed@gmail.com>
2024-08-06 15:45:29 +0300

    yarab

--- app/api/prompt/route.js
+++ app/api/prompt/route.js
@@ -1,11 +1,16 @@
 import { connectToDB } from "@utils/database";
 import Prompt from "@models/prompt";
 
-export const GET = async (req) => {
+export const GET = async (req, { searchParams }) => {
   try {
     await connectToDB();
 
-    const prompts = await Prompt.find();
+    const filter = {};
+    if (searchParams.has("tag")) {
+      filter.tag = searchParams.get("tag");
+    }
+
+    const prompts = await Prompt.find(filter).populate("creator");
     if (!prompts) return new Response("Prompts not found", { status: 404 });
     return new Response(JSON.stringify(prompts), { status: 200 });
   } catch (err) {
